8e2f9fd96f993d6e7ebcf5cb719f570f49762f39
Ivan Carcamo <ivandres73@unitec.edu>
2019-11-23 14:37:00 -0600

    created login.js

---
+++ src/components/Login.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Navigation extends Component {
+    render() {
+        return(
+            <form className="form-signing">
+                <input id="username" class="form-control" placeholder="username" required="" autofocus=""></input>
+
+            </form>
+        )
+    }
+}
