3e8220e354759827f5f1fa97817bccc840e4a178
Ivan Carcamo <ivandres73@unitec.edu>
2019-11-25 15:16:51 -0600

    handling inputs in form

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,12 +1,41 @@
 import React, { Component } from 'react';
 
 class Signup extends Component {
+    constructor() {
+        super();
+        this.state = {
+
+        }
+    }
+
+    handleInput(e) {
+        console.log(e.target.name, e.target.value);
+    }
+
     render() {
         return(
             <div>
                 <form className="form-signing">
-                    <input id="username" className="form-control" placeholder="username" required="" autoFocus=""></input>
-                    <input type="password" id="inputPassword" className="form-control" placeholder="Password" required=""></input>
+                    <input
+                        name="username"
+                        className="form-control"
+                        placeholder="username"
+                        onChange={this.handleInput}
+                    />
+                    <input
+                        type="password"
+                        name="password"
+                        className="form-control"
+                        placeholder="Password"
+                        onChange={this.handleInput}
+                    />
+                    <input
+                        type="password"
+                        name="password confirm"
+                        className="form-control"
+                        placeholder="Password Confirmation"
+                        onChange={this.handleInput}
+                    />
                 </form>
                 <button className="btn btn-lg btn-primary btn-block" type="submit">Sign up</button>
             </div>
