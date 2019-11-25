bfd735ac7460efa7c41848925077f79ed40531ed
Ivan Carcamo <ivandres73@unitec.edu>
2019-11-25 14:34:09 -0600

    navbar dark theme

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import './App.css';
 
 //Custom Components and routes
-import Login from './components/Login';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from './components/Login';
 import Signup from './components/Signup';
+import Navbar from './components/Navbar';
 
 function App() {
   console.log('hola soy ivan');
   return (
     <div className="App">
+      <Navbar/>
       <BrowserRouter>
       <Route exact={true} path='/' render={() => (<Login/> )}/>
       <Route exact={true} path='/signup' render={() => (<Signup/> )}/>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,15 @@ import React, { Component } from 'react';
 class Navbar extends Component {
     render() {
         return(
-            <div className="sigup-comp">
-                
+            <div>
+                <nav className="navbar navbar-dark bg-dark">
+                    <span className="navbar-text text-white">
+                        User
+                    </span>
+                    <a href="#" className="text-white">
+                        Log out
+                    </a>
+                </nav>
             </div>
         )
     }
