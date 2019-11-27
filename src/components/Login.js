cd5abaca35e898d5eeff660ef7d7e8e5b9c56f03
Ivan Carcamo <ivandres73@unitec.edu>
2019-11-27 00:47:02 -0600

    post for user validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,21 +6,26 @@ class Login extends Component {
         super(props);
 
         this.state = {
-            users: [],
-            local_username: '',
-            local_password: ''
+            username: '',
+            password: ''
         }
         this.handleInput = this.handleInput.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
     }
 
-    logIn() {
-        axios.get('http://localhost/user/'+this.state.local_username)//line changed in production
+    handleLogin() {
+        console.log('state', this.state);
+        axios.post('http://localhost/login/', this.state)//line changed in production
         .then(response => {
-            console.log('response: ',response.data)
-            //this.setState({users: response.data})
+            console.log('response: ', response.data)
+            if (response.data.message == 'invalid password')
+                alert('Incorrect password');
+            else if (response.data.username != null)
+                alert('Loggin Success');
+            else
+                alert('User doesnt exist');
         }).catch(error => {
             console.error(error)
-            alert('User not found');
         })
     }
 
@@ -29,18 +34,15 @@ class Login extends Component {
         this.setState({
             [name]: value
         });
-        console.log('users: ', this.state.local_username);
+        console.log('users: ', this.state.username);
     }
 
-    render() {
-        //console.log(this.state.users);
-        const users = this.state.users;
-        
+    render() {        
         return(
             <div>
                 <form className="form-signing">
                     <input
-                        name="local_username"
+                        name="username"
                         className="form-control"
                         placeholder="Username"
                         autoFocus=""
@@ -48,19 +50,16 @@ class Login extends Component {
                     />
                     <input
                         type="password"
-                        name="local_password"
+                        name="password"
                         className="form-control"
                         placeholder="Password"
                         onChange={this.handleInput}
                     />
                 </form>
-                <button className="btn btn-lg btn-primary btn-block" type="submit" onClick={this.logIn}>Log in</button>
+                <button className="btn btn-lg btn-primary btn-block" type="submit" onClick={this.handleLogin}>Log in</button>
                 <p>
                     Don't have an account? <a href="/signup">Sign up</a>
                 </p>
-                {
-                    users.map(a => <h1>{a.username}</h1>)
-                }
             </div>
         )
     }
