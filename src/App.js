85de55b50fa58c9ca616d8fa4dc9c9e2af345d10
Ivan Carcamo <ivandres73@unitec.edu>
2019-11-27 22:07:29 -0600

    trying to center the form

--- src/App.js
+++ src/App.js
@@ -9,12 +9,21 @@ import CharacterPage from './components/CharacterPage';
 
 function App() {
   console.log('hola soy ivan');
+  const stylish={justifyContent: 'center', textAlign: 'center', align:'center'}
   return (
     <div className="App">
       <BrowserRouter>
         <Route exact={true} path='/home' render={() => (<CharacterPage/> )}/>
-        <Route exact={true} path='/' render={() => (<Login/> )}/>
-        <Route exact={true} path='/signup' render={() => (<Signup/> )}/>
+        <Route exact={true} path='/' render={() => (
+          <div className="col">
+            <Login/>
+          </div>
+        )}/>
+        <Route exact={true} path='/signup' render={() => (
+          <div className="col">
+            <Signup/>
+          </div>
+        )}/>
         <Route exact={true} path='/login' render={() => (<Login/> )}/>
       </BrowserRouter>
     </div>
